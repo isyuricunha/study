856d3b452c34bb75c6d9605a1c460415a32b9da1
Yuri <isyuricunha@duck.com>
2022-11-18 09:00:00 -0300

    sep func

---
+++ python/functions print and variable/sep.py
@@ -0,0 +1,10 @@
+print('Brazil have 5 titles.') # Show the string Brazil have 5 titles.
+
+print('Brazil have 5 titles.', sep=' ') # Show the string Brazil have 5 titles.
+
+print('Brazil', 'have', 5, 'titles.', sep='-') # Show the string Brazil-have-5-titles.
+
+print('Brazil', 'have', 5, 'titles.', end='\n') # Show the string Brazil-have-5-titles.
+
+print('Brazil', 'have', 5, 'titles.', end='END') # Show the string Brazil-have-5-titles.
+
