d8f7caf6396a31385588cb776a8481129c6e1839
Yuri <isyuricunha@duck.com>
2022-11-18 09:11:14 -0300

    changes variables value and print like a text

--- python/functions print and variable/varable.py
+++ python/functions print and variable/varable.py
@@ -6,4 +6,12 @@ print(country); # Italy
 
 amount = 4; # variable
 
+type(amount); # <class 'int'>
+
 print(country, 'have', amount, 'world titles.', sep=' '); # Italy have 4 titles.
+
+country = 'France'; # variable
+
+type(country); # <class 'str'>
+
+print(country); # France
