f492ed58e098ce5f0157ada5dfae375213eb42cc
Yuri <isyuricunha@duck.com>
2022-11-18 08:50:18 -0300

    add first archive

---
+++ python/functions print and variable/funcAndVariables.py
@@ -0,0 +1,7 @@
+print('Hello World') # Show the string Hello World
+
+help() # Show the help. Help is a function.
+
+help>print # Show the help of the function print
+
+print(...) # Show the help of the function print
