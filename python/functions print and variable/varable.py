11c86eeab82e5a12cf23aca82b69a935f1354c88
Yuri <isyuricunha@duck.com>
2022-11-18 09:06:21 -0300

    variabels

---
+++ python/functions print and variable/varable.py
@@ -0,0 +1,9 @@
+country = 'Italy'; # variable
+
+type(country); # <class 'str'>
+
+print(country); # Italy
+
+amount = 4; # variable
+
+print(country, 'have', amount, 'world titles.', sep=' '); # Italy have 4 titles.
