31393f6839385c7edd9ee71dc81e4d9bbdfb2c35
Yuri Cunha <isyuricunha@duck.com>
2023-05-14 09:43:00 -0300

    primeiro movido para basicos

--- next-and-react/exercises/pages/basicos/primeiro.tsx
+++ next-and-react/exercises/pages/basicos/primeiro.tsx
@@ -1,7 +1,7 @@
 export default function primeiro() { // Função que será exportada
-    return "Primeiro alterado" // Retorno da função
+    return "Primeiro" // Retorno da função
 } // Fim da função
 
-// url: http://localhost:3000/primeiro
-// Retorno: Primeiro alterado
+// url: http://localhost:3000/basicos/primeiro
+// Retorno: Primeiro
 // Ao criar uma função, é necessário exportá-la para que ela possa ser utilizada em outros arquivos
