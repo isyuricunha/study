11fa3dde610f3c544006e1435f0667369a74d8ec
Yuri Cunha <isyuricunha@duck.com>
2023-05-20 17:14:57 -0300

    jsx1 and jsx2

---
+++ next-and-react/exercises/pages/basicos/jsx1.tsx
@@ -0,0 +1,9 @@
+// JSX 
+
+export default function segundo() {
+    return (
+        <div>
+            <h1>Segundo</h1>
+        </div>
+    )
+}
---
+++ next-and-react/exercises/pages/basicos/jsx2.tsx
@@ -0,0 +1,9 @@
+export default function jsx2() {
+  const conteudo = (
+    <div>
+      <h1>JSX #02</h1>
+    </div>
+  );
+
+  return conteudo;
+}
